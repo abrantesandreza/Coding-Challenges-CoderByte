e97a92cb1459d97453b7f1da265d0c0938ac1aa8
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-03-13 00:35:49 -0300

    Coding challenge AgeEnough

--- AgeEnough/Program.cs
+++ AgeEnough/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Write a program that checks if the user is old enough to drive
+
+        Console.WriteLine("What's your age?");
+        int userAge = int.Parse(Console.ReadLine());
+
+        if (userAge < 16)
+        {
+            Console.WriteLine($"You are {userAge}, you can't drive yet!");
+        }
+        else
+        {
+            Console.WriteLine($"You are {userAge}, you're allowed to drive!");
+        }
+    }
+}
